95c687d594efd16483373013c006879660937e17
Felixx20 <f-email.1@web.de>
2022-07-22 14:28:44 +0200

    added new Routing and Controller

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -10,6 +10,25 @@ return [
             Controller\BlogController::class => InvokableFactory::class,
         ],
     ],
+
+    'router' => [
+        'routes' => [
+            'blog' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/blog[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\BlogController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
     'view_manager' => [
         'template_path_stack' => [
             'blog' => __DIR__ . '/../view',
